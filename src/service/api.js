ab1aea74bc45ff7f1f5db7b468b876c957868ce6
curiousGeekStud <curiousGeekStud@gmail.com>
2025-02-19 00:24:11 +0530

    backend url updated

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
+// const URL = 'http://localhost:8000';
+const URL = 'https://flipkart-server-delta.vercel.app:8000'
 
 export const authenticateSignup = async(data) =>{
     try{
